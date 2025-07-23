9f9547ba90c162d9dbc0098733619690b2683c95
hdilg <abdulelahhdilg502@gmail.com>
2025-07-23 21:13:03 +0300

    Delete geo-redirect.js

--- geo-redirect.js
+++
@@ -1,9 +0,0 @@
-export default async (request, context) => {
-  const country = context.geo?.country?.code;
-
-  if (country === "SA") {
-    return Response.redirect("https://sicklv.shop/sa-access", 302);
-  }
-
-  return context.next();
-};
